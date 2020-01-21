6dfb15e22e3b9d23e552e86c0f6dbc7b65f0f4e9
cybertek <github@cybertekschool.com>
2020-01-21 16:37:36 -0500

    day 4 parameters 2

--- src/test/java/com/vytrack/step_definitions/Hooks.java
+++ src/test/java/com/vytrack/step_definitions/Hooks.java
@@ -1,8 +1,11 @@
 package com.vytrack.step_definitions;
 
 import com.vytrack.utilities.Driver;
+import io.cucumber.core.api.Scenario;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public class Hooks {
 
@@ -22,7 +25,13 @@ public class Hooks {
     }
 
     @After
-    public void tearDown(){
+    public void tearDown(Scenario scenario){
+        //if the scenario fails take the screenshot
+        if(scenario.isFailed()){
+            final byte[] screenshot = ((TakesScreenshot) Driver.get()).getScreenshotAs(OutputType.BYTES);
+            scenario.embed(screenshot,"image/png");
+        }
+
         Driver.closeDriver();
     }
 
--- src/test/resources/features/Contacts.feature
+++ src/test/resources/features/Contacts.feature
@@ -11,12 +11,20 @@ Feature: Contacts Page
     And the user enter the sales manager information
     When the user navigates "Fleet" "Vehicles"
 
-  @wip
+
   Scenario: Menu options
     Given the user logged in as a "driver"
     Then the user should see following menu options
-      | Fleet      |
-      | Customers  |
-      | Activities |
-      | System     |
+      | Fleet | Customers | Activities | System |
+
+  @wip
+  Scenario: login as a given user
+    Given the user is on the login page
+    When the use logs in using following credentials
+      | username  | user1       |
+      | password  | UserUser123 |
+      | firstname | John        |
+      | lastname  | Doe         |
+    Then the user should be able to login
+
 
