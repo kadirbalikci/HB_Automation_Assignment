cfd3915c55d643377c1740c87ddfaa81a2c7d6ea
cybertek <github@cybertekschool.com>
2020-02-04 17:17:42 -0500

    test final branch

--- src/test/java/com/vytrack/runners/FailedTestRunner.java
+++ src/test/java/com/vytrack/runners/FailedTestRunner.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = {"html:target/failed-html-reports"},
         features = "@target/rerun.txt",
         glue = "com/vytrack/step_definitions"
 )
